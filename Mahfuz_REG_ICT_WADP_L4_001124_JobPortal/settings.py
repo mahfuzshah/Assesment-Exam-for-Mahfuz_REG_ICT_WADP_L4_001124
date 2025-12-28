d7514d199d4dab81378024a0e521eee6b2f09412
Mahfuz Shah <mahfuzshah17@gmail.com>
2025-12-28 16:27:55 +0600

    Uodate

--- Mahfuz_REG_ICT_WADP_L4_001124_JobPortal/settings.py
+++ Mahfuz_REG_ICT_WADP_L4_001124_JobPortal/settings.py
@@ -29,13 +29,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = 'Mahfuz_REG_ICT_WADP_L4_001124_JobPortal.urls'
@@ -103,7 +103,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = "static/"
+
 
 
 # Default primary key field type
